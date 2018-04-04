92bded1d71e0aed7f10c06134e844003f14078cb
pkgyan-baffour <pkgyan-baffour@uclan.ac.uk>
2018-04-04 09:40:33 +0100

    Kev-Moji Update

---
+++ Kev-moji.js
@@ -0,0 +1,34 @@
+//scroll for JS//
+$(document).ready(function() {
+  // Add smooth scrolling to all links
+  $("a").on("click", function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
+
+//assign() method if you want to load a new document,
+//and the option to navigate back to the original document.
+//DOM Location..
+function myFunction() {
+  location.assign("Kev-moji.html");
+}
